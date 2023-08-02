1661dab1945deb459cb35bbabc55e5d5f5659185
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-08-02 16:14:29 +0300

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 greetings = [
-  "Hello, World!",
-  "Welcome to our app!",
-  "Greetings from Rails!",
-  "Hey there!",
-  "Hello, how are you?"
+  "Hello, World !",
+  "Welcome to our app !",
+  "Greetings from Rails !",
+  "Hey there !",
+  "Hello, how are you ?"
 ]
 
 greetings.each do |greeting|
